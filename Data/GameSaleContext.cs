8090389a36a52d6e3a5d5756fb7d9fdc435ba86f
Savvas <smorion@gmail.com>
2019-05-28 18:16:35 +0300

    Initial commit

---
+++ Data/GameSaleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameSale.Models
+{
+    public class GameSaleContext : DbContext
+    {
+        public GameSaleContext (DbContextOptions<GameSaleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<GameSale.Models.Game> Game { get; set; }
+    }
+}
---
+++ Models/Game.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameSale.Models
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public string Price { get; set; }
+    }
+}
